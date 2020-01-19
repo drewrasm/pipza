81daf526549467bc7e020337dc6598247d32b0b7
Andrew <drewrasm@gmail.com>
2020-01-19 00:29:42 -0700

    worked on adding config example and working it into the actual program

--- pipza.py
+++ pipza.py
@@ -1,10 +1,18 @@
 import pizzapi
-
+import json
+import sys
 
 print('Welcome to the best way to order your Domino\'s pizza')
-print('_____________________________________________________')
+print('_________________________')
 
-def getCustomer(): 
+try: 
+    f =  open('config.json')
+except: 
+    print('you do not have a settings file set up, if you would like one, please make a file called config.json, an example is provided.' + '\n')
+    print('_________________________')
+
+def getCustomer(mode): 
+    #check for each of the needed variables if it exists inside of the config file, if not just use the custom ones
     print('Your customer info: ')
     fullName = input('Enter your first and last name (ex: Babe Ruth): ')
     firstName = fullName.split(' ')[0]
@@ -13,7 +21,8 @@ def getCustomer():
     phone = input('Please enter your phone number: ')
     return pizzapi.Customer(firstName, lastName, email, phone)
 
-def getAddress():
+def getAddress(mode):
+    #check for each of the needed variables if it exists inside of the config file, if not just use the custom ones
     print('Your address: ')
     street = input('Please enter your street (ex: 299 east example way): ')
     city = input('Please enter your city: ')
@@ -21,12 +30,29 @@ def getAddress():
     zipCode = input('Please enter your zip code: ')
     return pizzapi.Address(street, city, state, zipCode)
     
-def getStore():
-    return getAddress().closest_store()
+def getStore(address):
+    return address.closest_store()
 
-customer = getCustomer()
-store = getStore()
+# customer = getCustomer()
+# store = getStore()
 
-print('Hello, ' + customer.first_name + ' ' + customer.last_name)
-print('Your closest store is: ' + store)
+# print('Hello, ' + customer.first_name + ' ' + customer.last_name)
+# print('Your closest store is: ' + store)
+mode = ''
+for i in range(len(sys.argv)):
+    if(sys.argv[i] == '-default'):
+        #use all the available file values
+        mode = 'default'
+        break
+    if(sys.argv[i] == '-custom'): 
+        #use inputs
+        mode = 'custom'
+        break
+    if(sys.argv[i] == '-help'):
+        print('here are the available commands: ' + '\n' + '-help (displays the available arguments)' + '\n' + '-default (reads available values from config file to the program)' + '\n' + '-custom (allows for full custom inputs for order)' )
+        break
+
+
+
+    
 
