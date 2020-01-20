f5fe8145ea65bc00020c002b341f96c772cc5ed0
Andrew <drewrasm@gmail.com>
2020-01-19 17:15:10 -0700

    worked on pulling from file

--- pipza.py
+++ pipza.py
@@ -13,31 +13,50 @@ except:
 
 def getCustomer(mode): 
     #check for each of the needed variables if it exists inside of the config file, if not just use the custom ones
-    print('Your customer info: ')
-    fullName = input('Enter your first and last name (ex: Babe Ruth): ')
-    firstName = fullName.split(' ')[0]
-    lastName = fullName.split(' ')[1]
-    email = input('Please enter your email: ')
-    phone = input('Please enter your phone number: ')
+    try: 
+        with open('config.json') as json_file:
+            customer = json.load(json_file).get('customer')
+    except: 
+        customer = ''
+
+    if(customer != '' and mode !='-custom'):
+        firstName = customer.get('first_name')
+        lastName = customer.get('last_name')
+        email = customer.get('email')
+        phone = customer.get('phone')
+    else: 
+        print('Your customer info: ')
+        fullName = input('Enter your first and last name (ex: Babe Ruth): ')
+        firstName = fullName.split(' ')[0]
+        lastName = fullName.split(' ')[1]
+        email = input('Please enter your email: ')
+        phone = input('Please enter your phone number: ')
     return pizzapi.Customer(firstName, lastName, email, phone)
 
 def getAddress(mode):
     #check for each of the needed variables if it exists inside of the config file, if not just use the custom ones
-    print('Your address: ')
-    street = input('Please enter your street (ex: 299 east example way): ')
-    city = input('Please enter your city: ')
-    state = input('Please enter your state (ex: \'UT\' for Utah): ')
-    zipCode = input('Please enter your zip code: ')
+    try: 
+        with open('config.json') as json_file:
+            address = json.load(json_file).get('address')
+    except: 
+        address = ''
+
+    if(address != '' and mode !='-custom'):
+        street = address.get('street')
+        city = address.get('city')
+        state = address.get('state')
+        zipCode = address.get('zip_code')
+    else: 
+        print('Your address: ')
+        street = input('Please enter your street (ex: 299 east example way): ')
+        city = input('Please enter your city: ')
+        state = input('Please enter your state (ex: \'UT\' for Utah): ')
+        zipCode = input('Please enter your zip code: ')
     return pizzapi.Address(street, city, state, zipCode)
     
 def getStore(address):
     return address.closest_store()
 
-# customer = getCustomer()
-# store = getStore()
-
-# print('Hello, ' + customer.first_name + ' ' + customer.last_name)
-# print('Your closest store is: ' + store)
 mode = ''
 for i in range(len(sys.argv)):
     if(sys.argv[i] == '-default'):
@@ -49,8 +68,19 @@ for i in range(len(sys.argv)):
         mode = 'custom'
         break
     if(sys.argv[i] == '-help'):
+        print('*only one command is valid at a time*')
         print('here are the available commands: ' + '\n' + '-help (displays the available arguments)' + '\n' + '-default (reads available values from config file to the program)' + '\n' + '-custom (allows for full custom inputs for order)' )
         break
+
+customer = getCustomer(mode)
+address = getAddress(mode)
+store = getStore(address)
+
+print('Hello, ' + customer.first_name + ' ' + customer.last_name)
+print('Your store is : ', store)
+
+
+
 
 
 
