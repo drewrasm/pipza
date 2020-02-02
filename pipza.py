615fbea00fd6551bf32f374ca1129e6180efa643
Andrew Rasmussen <andrew.rasmussen1@aggiemail.usu.edu>
2020-02-02 15:16:12 -0700

    working on parsing menu

--- pipza.py
+++ pipza.py
@@ -20,10 +20,21 @@ def getCustomer(mode):
         customer = ''
 
     if(customer != '' and mode !='custom'):
-        firstName = customer.get('first_name')
-        lastName = customer.get('last_name')
-        email = customer.get('email')
-        phone = customer.get('phone')
+        try:
+            firstName = customer.get('first_name')
+            lastName = customer.get('last_name')
+        except:
+            fullName = input('Enter your first and last name (ex: Babe Ruth): ')
+            firstName = fullName.split(' ')[0]
+            lastName = fullName.split(' ')[1]
+        try:
+            email = customer.get('email')
+        except:
+            email = input('Please enter your email: ')
+        try:
+            phone = customer.get('phone')
+        except: 
+            phone = input('Please enter your phone number: ')
     else: 
         print('Your customer info: ')
         fullName = input('Enter your first and last name (ex: Babe Ruth): ')
@@ -57,6 +68,46 @@ def getAddress(mode):
 def getStore(address):
     return address.closest_store()
 
+def displayStoreData(store):
+    details = store.get_details()
+    storeId = details['StoreID']
+    streetName = details['StreetName']
+    city = details['City']
+    region = details['Region']
+    phone = details['Phone']
+    print(f'Your store is :  ID: { storeId }, ADDRESS: {streetName}, {city}, {region}')
+    print(f'Their phone number is: {phone}')
+
+def getCard():
+    try: 
+        with open('config.json') as json_file:
+            card = json.load(json_file).get('card')
+    except: 
+        card = ''
+
+    if(card != '' and mode !='custom'):
+        cardNumber = card.get('card_number')
+        if(cardNumber == ''):
+            cardNumber = input('Please enter your card number: ')
+        pin = card.get('pin')
+        if(cardNumber == ''):
+            pin = input('Please enter your pin: ')
+        zipCode = card.get('zip')
+        if(zipCode == ''):
+            zipCode = input('Please enter the zip code: ')
+        expiration = card.get('expiration')
+        if(expiration == ''):
+            expiration = input('Please enter the expiration date: ')
+    else: 
+        print('Your card: ')
+        cardNumber = input('Please enter your card number: ')
+        pin = input('Please enter your pin: ')
+        zipCode = input('Please enter the zip code: ')
+        expiration = input('Please enter the expiration date: ')
+    return pizzapi.PaymentObject(cardNumber, expiration, pin, zipCode)
+    
+
+
 mode = ''
 for i in range(len(sys.argv)):
     if(sys.argv[i] == '-default'):
@@ -72,18 +123,36 @@ for i in range(len(sys.argv)):
         print('here are the available commands: ' + '\n' + '-help (displays the available arguments)' + '\n' + '-default (reads available values from config file to the program)' + '\n' + '-custom (allows for full custom inputs for order)' )
         break
 
+
 customer = getCustomer(mode)
 address = getAddress(mode)
 store = getStore(address)
+# card = getCard()
+
+# print('did card work?', card)
+
+
+if(not store.data['IsOpen']):
+    print('warning, the closest store to you is closed, you may want to call them')
+menu = store.get_menu()
+codes = menu.menu_by_code
+for code in codes:
+    currentData = codes[code].menu_data
+    # if 'Large' in currentData['Size']:
+        # print(currentData)
+    # print(currentData)
+    if('Size' in currentData.keys() and 'Large' in currentData['Size']):
+        print(currentData['Description'])
+        
+
 
 order = pizzapi.Order(store, customer, address)
-#TODO: make a function that will gather the card informtion. maybe we should gather the card information after they have chosen their pizza
 #TODO: display their total bill before they pay for it
 #TODO: maybe give them an option to do a test without using actual card information
 
 
 print('Hello, ' + customer.first_name + ' ' + customer.last_name)
-print('Your store is : ', store)
+displayStoreData(store)
 
 
 
